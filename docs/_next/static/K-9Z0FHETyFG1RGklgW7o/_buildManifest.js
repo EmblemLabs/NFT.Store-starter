acfee36e1c289cc84eb391707f197ea56c0d83c9
shannon code <genecyber@gmail.com>
2021-07-23 15:16:07 -0400

    deploy pages

--- docs/_next/static/K-9Z0FHETyFG1RGklgW7o/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-369f3ea9dc503123ca26.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-aeac9df3c1f25dac4810.js"],"/main":["static\u002Fcss\u002Fcf4d4fd58988b93bafc1.css","static\u002Fchunks\u002Fpages\u002Fmain-275bbae56aa970e1e09d.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bbf1df9228e52728a6e1";
+/******/ 	var hotCurrentHash = "7651c94edec0d48b8681";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
